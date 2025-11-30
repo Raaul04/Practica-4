6b4127faab53a1a3a434f8b22ea712aaff637889
jorgefdeez <jfernandezg20@alumnos.nebrija.es>
2025-11-30 13:40:42 +0100

    query & mutation(addpost, login & register)

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,7 +1,64 @@
+
 import { IResolvers } from "@graphql-tools/utils";
+import { getDB } from "../db/mongo";
+import { ObjectId } from "mongodb";
+import { createUser } from "../collections/userPosts";
+import { signToken } from "../auth";
+import { validateUser } from "../collections/userPosts";
 
+const COLLECTION = "Posts";
 
+export const resolvers: IResolvers = {
+  Query: {
+    getPost: async () => {
+      const db = getDB();
+      return db.collection(COLLECTION).find().toArray();
+    },
+    getPostID: async (_, { _id }: { _id: string }) => {
+      const db = getDB();
+      return db.collection(COLLECTION).findOne({ _id: new ObjectId(_id) });
+    },
 
-export const resolvers:IResolvers={
-    
-}
+    me : async(_,__, {user})=>{
+      if(!user) return null
+      return {
+        id: user._id.toString(),
+        email: user.email
+      }
+    }
+  },
+
+  Mutation: {
+    addPost: async (_,{ titulo, contenido, autor,  fechaCreada}: { titulo: string; contenido: string; autor: string,  fechaCreada: string}) => {
+        const db = getDB();
+        const result = await db.collection(COLLECTION).insertOne({
+          titulo,
+          contenido,
+          autor,
+          fechaCreada
+        });
+
+        return {
+          _id: result.insertedId,
+          titulo,
+          contenido, 
+          autor,
+          fechaCreada
+        };
+
+    },
+
+    register: async(_, {email, password}: {email: string, password: string}) => {
+      const userId = await createUser(email, password)
+      return signToken(userId)
+    }, 
+
+    login: async(_, {email, password}: {email: string, password: string}) => {
+      const user = await validateUser(email, password)
+      if(!user){
+        throw new Error("Las credenciales no son correctas")
+      }
+      return signToken(user._id.toString())
+    } 
+  },
+};
