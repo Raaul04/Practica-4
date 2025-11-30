f85d21489f81c2a8a4f54537244f73eb522bd262
jorgefdeez <jfernandezg20@alumnos.nebrija.es>
2025-11-30 13:08:51 +0100

    update schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,60 @@
-import { gql } from "apollo-server";
-
-
+import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
+
+    type User{
+        _id:ID!,
+        email: srtring,
+        name:string,
+        password:string
+    }
+
+    type Post{
+        _id: ID!,
+        titulo:string,
+        contenido:string,
+        autor:string,
+        fechaCreada:string
+    }
+
+    type Query {
+
+        me: User
+        getPost: [Post]!
+        getPost(id : ID!): Post
+    }
+
+    type Mutation {
     
+        register(
+            email: string!,
+            password: string!
+        ) : string!
+
+        login(
+            email: string!,
+            password: string!
+        ) : string!
+
+        addPost(
+            titulo:string!,
+            contenido:string!,
+            autor:string!,
+            fechaCreada:string!
+        ): Post!
+
+        updatePost(
+            _id:ID!,
+            titulo:string,
+            contenido:string,
+            autor:string,
+            fechaCreada:string
+        ): Post!
+
+        deletePost(
+            _id:ID!
+        ): Post!
+
+    }
 
 `;
